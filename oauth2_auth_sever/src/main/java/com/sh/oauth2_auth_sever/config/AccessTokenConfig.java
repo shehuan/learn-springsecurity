46a4ab6051c8d3247f3de42b5640847249e96237
shehuan <shehuan@nowledgedata.com.cn>
2022-09-28 14:11:29 +0800

    RedisTokenStore

--- oauth2_auth_sever/src/main/java/com/sh/oauth2_auth_sever/config/AccessTokenConfig.java
+++ oauth2_auth_sever/src/main/java/com/sh/oauth2_auth_sever/config/AccessTokenConfig.java
@@ -1,9 +1,12 @@
 package com.sh.oauth2_auth_sever.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 /**
  * description：
@@ -12,12 +15,25 @@ import org.springframework.security.oauth2.provider.token.store.InMemoryTokenSto
 @Configuration
 public class AccessTokenConfig {
     /**
-     * 用来保存生成的 Access Token
+     * 将 Access Token 保存到内存
      *
      * @return
      */
+//    @Bean
+//    TokenStore tokenStore() {
+//        return new InMemoryTokenStore();
+//    }
+
+
+    /**
+     * 将 Access Token 保存到Redis
+     *
+     * @return
+     */
+    @Autowired
+    RedisConnectionFactory redisConnectionFactory;
     @Bean
     TokenStore tokenStore() {
-        return new InMemoryTokenStore();
+        return new RedisTokenStore(redisConnectionFactory);
     }
 }
